9dc67895321c8f7bc3fcae722b88325cb2cbcfda
DonMartinWorks <donmartinworks@gmail.com>
2023-09-13 11:27:07 -0300

    [Admin Login] - Creación de la vista de acceso ADMIN

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -7,8 +7,19 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    /**
+     * return admin dashboard.
+     */
     public function dashboard()
     {
         return view('roles.admin.dashboard');
+    }
+
+    /**
+     * return admin login
+     */
+    public function login()
+    {
+        return view('roles.admin.auth.login');
     }
 }
