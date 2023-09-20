76bef857058c8197ba5fc4e52df34a908edd0a67
DonMartinWorks <donmartinworks@gmail.com>
2023-09-20 12:21:50 -0300

    [Sidebar] - Modificación del sidebar

--- resources/views/roles/admin/layouts/sidebar.blade.php
+++ resources/views/roles/admin/layouts/sidebar.blade.php
@@ -1,20 +1,23 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">{{ config('app.name', 'Laravel') }}</a>
+            <a href="/">{{ config('app.name', 'Laravel') }}</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">+</a>
+            <a href="/">+</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="#" class="nav-link has-dropdown"><i
+                        class="fas fa-fire"></i><span>{{ __('Dashboard') }}</span></a>
                 <ul class="dropdown-menu">
-                    <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                    <li class=active><a class="nav-link"
+                            href="{{ route('admin.dashboard') }}">{{ __('General Admin Dashboard') }}</a></li>
                     <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                 </ul>
             </li>
+
             <li class="menu-header">Starter</li>
             <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
@@ -25,6 +28,7 @@
                     <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
                 </ul>
             </li>
+
             <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank
                         Page</span></a></li>
         </ul>
