164647bd0a61259044618e0fd2dba2b618906453
DonMartinWorks <donmartinworks@gmail.com>
2023-09-22 11:58:39 -0300

    [Forgot Password] - Cambio de diseño en la vista de olvidaste la contraseña

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,6 +36,9 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        $tra = __('We have emailed your password reset link.');
+        toastr()->info($tra);
+
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
