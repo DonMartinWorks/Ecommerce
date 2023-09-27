4a5b85bec8479c8e23b1f8d8341edf522eeb04dd
DonMartinWorks <donmartinworks@gmail.com>
2023-09-27 15:10:25 -0300

    [Profile] - Creación de la vista PROFILE

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\frontend\HomeController;
+use App\Http\Controllers\Frontend\UserProfileController;
 use App\Http\Controllers\Frontend\UserDashboardController;
 
 /*
@@ -19,8 +20,6 @@ use App\Http\Controllers\Frontend\UserDashboardController;
     Roles
  */
 
-// $admin = __('admin');
-// $vendor = __('vendor');
 $user = __('user');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -28,6 +27,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 //Esto es para redireccionar a los usuarios que NO SEAN admin/vendor a dashboard
 Route::group(['middleware' => ['auth', 'verified'], 'prefix' => $user, 'as' => 'user.'], function () {
     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [UserProfileController::class, 'index'])->name('profile');
 });
 
 Route::middleware('auth')->group(function () {
