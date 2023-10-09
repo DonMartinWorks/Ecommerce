da6c51a938e07799a3ca3aee9a6f062166dfd923
DonMartinWorks <donmartinworks@gmail.com>
2023-10-09 15:24:05 -0300

    [Slider] - Inicio

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\ProfileController;
 
 /*
@@ -27,3 +28,14 @@ Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard'
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'admin_profile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'password'])->name('password.update');
+
+
+/*
+|--------------------------------------------------------------------------
+| Slider
+|--------------------------------------------------------------------------
+|
+| Acá van las rutas de slider.
+|
+*/
+Route::resource('/slider', SliderController::class);
