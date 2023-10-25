a04e274bbe8b6451b005815992b4400570252398
DonMartinWorks <donmartinworks@gmail.com>
2023-10-25 16:11:58 -0300

    [Child Category] - Listado de las categorías hijas

--- app/Models/ChildCategory.php
+++ app/Models/ChildCategory.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ChildCategory extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the category that owns the ChildCategory
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Get the sub-category that owns the ChildCategory
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function subCategory(): BelongsTo
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
 }
